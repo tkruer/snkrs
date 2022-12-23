3414fddd9d813ad93c7962f8b105390042a91d31
Tyler K <tylerkruer1@gmail.com>
2022-12-23 10:12:12 -0700

    webhook.py added

--- src/webhook.py
+++ src/webhook.py
@@ -1,5 +1,43 @@
 import requests
+import log
 
-
-def sendWebhook():
-    return
+def send_webhook(webhook: str, store: str, sku: str, size: str, mode: str, email: str):
+    data = {
+      "content": "",
+      "embeds": [
+        {
+          "color": 7995224,
+          "fields": [
+            {
+              "name": "Store",
+              "value": f"`{store}`"
+            },
+            {
+              "name": "SKU",
+              "value": f"`{sku}`"
+            },
+            {
+              "name": "Size",
+              "value": f"`{size}`"
+            },
+            {
+              "name": "Email",
+              "value": f"||{email}||"
+            },
+            {
+              "name": "Mode",
+              "value": f"`{mode}`"
+            }
+          ],
+          "author": {
+            "name": "SNKRS Bot Success"
+          }
+        }
+      ],
+      "attachments": []
+    }
+    response = requests.post(url=webhook, json=data)
+    if response.status_code == 204:
+        print(f"Webhook sent to {webhook}")
+    else:
+        print(f"Webhook failed to send to {webhook}")
